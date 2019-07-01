92e385a44f2b195f487d3ec1d8560d8db05188a6
Tobiooo <tobiasoberbeck@gmx.de>
2019-07-01 22:22:37 +0200

    changes margin of register form. vs code theme of server. package-lock

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,42 +1,32 @@
 <template>
   <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">        
+    <v-flex mx-5>
+      <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>          
+          <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
-          
           <h1>Register</h1>
-  
-            <v-text-field 
-              label="Email"
-              v-model="email"
-            ></v-text-field>
-           
-            <v-text-field 
-              label="password"
-              @keydown.enter="register"
-              type="password" name="password" 
-              placeholder="password"
-              v-model="password"
-            ></v-text-field>      
 
-          <v-btn
-           @click="register"
-           dark
-           class="cyan"
-           >Register</v-btn>
+          <v-text-field label="Email" v-model="email"></v-text-field>
+
+          <v-text-field
+            label="password"
+            @keydown.enter="register"
+            type="password"
+            name="password"
+            placeholder="password"
+            v-model="password"
+          ></v-text-field>
+
+          <v-btn @click="register" dark class="cyan">Register</v-btn>
 
           <v-alert
             :value="errorMessage"
             type="error"
             transition="scale-transition"
-            icon="new_releases"            
-          >
-            {{ errorMessage }}
-          </v-alert>
-
+            icon="new_releases"
+          >{{ errorMessage }}</v-alert>
         </div>
       </div>
     </v-flex>
@@ -44,24 +34,24 @@
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService';
+import AuthenticationService from "@/services/AuthenticationService";
 
 export default {
-  name: 'Register',
+  name: "Register",
   data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       errorMessage: null,
       alert
     };
   },
-  computed:{
-    isError: () => this.errorMessage? false : true
+  computed: {
+    isError: () => (this.errorMessage ? false : true)
   },
   methods: {
     async register() {
-      try{
+      try {
         const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
@@ -70,8 +60,8 @@ export default {
         this.errorMessage = null;
 
         console.log(response.data);
-      } catch(error){
-        console.log('error', error)
+      } catch (error) {
+        console.log("error", error);
         this.errorMessage = error.response.data.error;
       }
     }
@@ -80,7 +70,7 @@ export default {
 </script>
 
 <style scoped>
-input{
+input {
   border: solid black 1px;
   border-radius: 4px;
   padding: 4px;
